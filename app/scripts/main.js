31f6163ec3b38aeccc79763c29cd79a39ecb8541
Luca Costabello <lukostaz@gmail.com>
2014-12-14 11:22:54 +0000

    First serializations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+console.log('Prissma Studio started.');
+
+
+$(document).ready(function(){
+
+	$("#generateButton").click(function() {
+		// var label = $("#inputName").val();
+
+		var writer = N3.Writer({ 'prissma': 'http://ns.inria.fr/prissma/v2#' });
+		writer.addTriple({
+			subject:   'http://example.org/prism1',
+			predicate: 'rdfs:label',
+			object:    '"' + $("#inputName").val() + '"'
+		});
+		writer.end(function (error, result) { console.log(result); });
+
+
+	});
+});
+
+
+
