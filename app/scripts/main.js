22236c93e6a2ba5bb45ab48425766509ad5dcd8c
Luca Costabello <lukostaz@gmail.com>
2014-12-14 15:20:32 +0000

    Added .ttl download

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,37 @@ console.log('Prissma Studio started.');
 $(document).ready(function(){
 
 	$("#generateButton").click(function() {
-		// var label = $("#inputName").val();
 
-		var writer = N3.Writer({ 'prissma': 'http://ns.inria.fr/prissma/v2#' });
-		writer.addTriple({
-			subject:   'http://example.org/prism1',
-			predicate: 'rdfs:label',
-			object:    '"' + $("#inputName").val() + '"'
+		var prismName = $("#inputName").val();
+
+		
+		var writer = N3.Writer();
+		writer.addPrefix('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
+		writer.addPrefix('prissma', 'http://ns.inria.fr/prissma/v2#');
+		writer.addTriple('http://example.org/prism1', 'prissma:name', '"' + prismName + '"');
+		
+
+
+		// serialize and download ttl file
+		writer.end(function (error, result) { 
+			console.log(result); 
+			window.URL = window.webkitURL || window.URL;
+			var contentType = 'text/turtle';
+			var csvFile = new Blob([result], {type: contentType});
+			var a = document.createElement('a');
+			a.download = prismName + '.ttl';
+			a.href = window.URL.createObjectURL(csvFile);
+			a.textContent = 'Download result';
+			a.dataset.downloadurl = [contentType, a.download, a.href].join(':');
+			a.click();
+
+
 		});
-		writer.end(function (error, result) { console.log(result); });
 
+
+
+
+		
 
 	});
 });
